4ff4b1c741aac0f1541267ae5509c789bf4e405b
WereWind <serg-ef@mail.ru>
2017-11-07 18:36:35 +0300

    Add a new test case

--- commentparser_test.cpp
+++ commentparser_test.cpp
@@ -66,4 +66,24 @@ SCENARIO("Rewriting single comments according to code style") {
       }
     }
   }
+
+  GIVEN(
+      "Doxygen styled multiline comments structured like /*! ... \n * ... "
+      "*/ ") {
+    QString wrong_source_code =
+        "/*!\\param   *comment1\n * \\param   comment2*/";
+
+    WHEN("Run the wrong code through comment parser") {
+      auto parsed_source_code =
+          CommentParser::RewriteCommentsAccordingToCodeStyle(wrong_source_code);
+      THEN(
+          "Parsed source code will have /// at the start (IDE colors them and "
+          "the stars in the middle will be gone") {
+        QString right_source_code =
+            "/// \\param   *comment1.\n  \\param   comment2.";
+        REQUIRE(parsed_source_code.toStdString() ==
+                right_source_code.toStdString());
+      }
+    }
+  }
 }
